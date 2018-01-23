c5b396ebd3bdb990b93e6ce974eed253ab8b8b2f
a-diamond <33737458+a-diamond@users.noreply.github.com>
2018-01-23 15:11:18 -0800

    Uploading my latest final project files, including .js file
    I don't have a ton of javascript needed/planned. Let me know if you think there are other ways I could/should incorporate.
    I also took a stab at messing around with some Bootstrap elements - and had a question about some kind of plug-in (see my note in the pseudo-code file)

---
+++ homework0/final-project/apps.js
@@ -0,0 +1,7 @@
+/*this is my jquery file
+
+ 1) When a user hovers over any nav bar items, create a highlight effect
+ 2) When a user clicks on a physician headshot, pop-up their bio
+
+ I want to use a tab structure from bootstrap for my nav, but it says I need to download a javascript plug-in. What is that about?
+*/
