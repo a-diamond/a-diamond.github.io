4de775a1eecf90d3506198cd00ee421f851e1912
a-diamond <33737458+a-diamond@users.noreply.github.com>
2018-01-30 18:35:12 -0800

    Uploading my javascript files
    Not sure that I will use any of this code. But let me know how it looks!

--- homework0/final-project/apps.js
+++ homework0/final-project/apps.js
@@ -1,7 +1,30 @@
 /*this is my jquery file
+1) When a user hovers over any nav bar items, create a highlight effect*/
+$('.nav-link').hover(){
+	$('this').css('background', 'yellow');
+}
 
- 1) When a user hovers over any nav bar items, create a highlight effect
- 2) When a user clicks on a physician headshot, pop-up their bio
+/* 2) When a user clicks on a physician headshot, pop-up their bio*/
+ $('.fox-bio').click() {
+$('#fox-bio').slideToggle();
+ }
 
- I want to use a tab structure from bootstrap for my nav, but it says I need to download a javascript plug-in. What is that about?
-*/
+$('.diamond-bio').click() {
+$('#diamond-bio').slideToggle();
+ }
+
+$('.rosenberg-bio').click() {
+$('#rosenberg-bio').slideToggle();
+ }
+
+$('.conigliari-bio').click() {
+$('#conigliari-bio').slideToggle();
+ }
+
+$('.okunola-bio').click() {
+$('#okunola-bio').slideToggle();
+ }
+
+$('.alias-bio').click() {
+$('#alias-bio').slideToggle();
+ }
